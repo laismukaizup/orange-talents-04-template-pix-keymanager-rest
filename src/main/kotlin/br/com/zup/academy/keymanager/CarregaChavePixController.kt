e345f1e7e012c8b5d34eff867492af360d9f1a6b
Lais Mukai <lais.mukai@zup.com.br>
2021-06-18 15:15:23 -0300

    implementado a exposição do detalhamento de chavePix

---
+++ src/main/kotlin/br/com/zup/academy/keymanager/CarregaChavePixController.kt
@@ -0,0 +1,30 @@
+package br.com.zup.academy.keymanager
+
+import br.com.zup.academy.CarregaChavePixGRPCServiceGrpc
+import br.com.zup.academy.CarregaChavePixRequest
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import io.micronaut.validation.Validated
+import javax.inject.Inject
+
+@Validated
+@Controller("/api/v1/clientes/{clienteId}")
+class CarregaChavePixController(@Inject private val carregaClient: CarregaChavePixGRPCServiceGrpc.CarregaChavePixGRPCServiceBlockingStub) {
+
+    @Get("pix/{pixId}")
+    fun carrega(clienteId: String, pixId: String): HttpResponse<Any> {
+
+        val response = carregaClient.carregar(
+            CarregaChavePixRequest.newBuilder()
+                .setPixId(
+                    CarregaChavePixRequest.FiltroPorIdPix.newBuilder()
+                        .setClienteId(clienteId)
+                        .setPixId(pixId).build()
+                )
+                .build()
+        )
+
+        return HttpResponse.ok(DetalheChavePixResponse(response))
+    }
+}
---
+++ src/main/kotlin/br/com/zup/academy/keymanager/DetalheChavePixResponse.kt
@@ -0,0 +1,28 @@
+package br.com.zup.academy.keymanager
+
+import br.com.zup.academy.CarregaChavePixResponse
+import io.micronaut.core.annotation.Introspected
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneOffset
+
+@Introspected
+class DetalheChavePixResponse(response: CarregaChavePixResponse) {
+    val pixId = response.pidId
+    val tipoChave = response.chave.tipo.name
+    val chave = response.chave.valorChave
+    val criadoEm = response.chave.criadoEm.let {
+        LocalDateTime.ofInstant(Instant.ofEpochSecond(it.seconds, it.nanos.toLong()), ZoneOffset.UTC)
+    }
+    val tipoConta = response.chave.conta.tipo.name
+    val conta = mapOf(Pair("tipo", tipoConta),
+        Pair("instituicao", response.chave.conta.instituicao),
+        Pair("nomeDoTitular", response.chave.conta.nomeDoTitular),
+        Pair("cpfDoTitular",  response.chave.conta.cpfDoTitular),
+        Pair("agencia",  response.chave.conta.agencia),
+        Pair("numero",  response.chave.conta.numeroDaConta),
+
+
+    )
+
+}
